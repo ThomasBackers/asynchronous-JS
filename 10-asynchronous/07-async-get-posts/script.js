3fc74f68ae5d3b2b6cf9da2793645e8c2d35784b
ThomasBackers <thomasbackers@gmail.com>
2021-10-18 02:47:19 +0200

    last commit

--- 10-asynchronous/07-async-get-posts/script.js
+++ 10-asynchronous/07-async-get-posts/script.js
@@ -10,7 +10,7 @@
 // You will have time to focus on it later.
 
 (() => {
-    document.querySelector("#run").addEventListener("click", () => {
-        ;
+    document.querySelector("#run").addEventListener("click", async() => {
+        await window.lib.getPosts();
     });
 })();
