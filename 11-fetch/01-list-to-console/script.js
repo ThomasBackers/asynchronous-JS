bb43edf1f71f3e98c8bc5f1f98731a183f6bac53
ThomasBackers <thomasbackers@gmail.com>
2021-10-18 13:36:20 +0200

    01-list-to-console done

--- 11-fetch/01-list-to-console/script.js
+++ 11-fetch/01-list-to-console/script.js
@@ -11,7 +11,7 @@
 
 (() => {
     document.querySelector("#run").addEventListener("click", async () => {
-        const response = await fetch("http://localhost:8000/heroes");
+        const response = await fetch("http://localhost:3000/heroes");
         console.log(await response.json());
 
         // Steps :
